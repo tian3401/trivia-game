4d0b2e4c3ba0609b90d0b6288867ece8344219b5
tian3401 <matt.chen3401@yahoo.com>
2018-06-29 12:50:04 -0700

    updated timing intervals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,112 +1,185 @@
 //Variables====================================================================
 var quesBank= [
     {
-        question: 'What is question 0.....',
-        choices: ['a','b','c','d'],
-        answer: 'blah blah'
+        question: 'What is John Snow\'s real name',
+        choices: ['Aegon Targaryen','John Stark','Rhaegar Targaryen','Robb Stark'],
+        answer: 'Aegon Targaryen'
     },
     {
-        question: 'What is .....',
-        choices: ['a','b','c','d'],
-        answer: 'blah blah'
+        question: 'What is the surname given to bastards born in Dorne?',
+        choices: ['Rivers','Sand','Waters','Stone'],
+        answer: 'Sand'
     },
     {
-        question: 'What is .....',
-        choices: ['a','b','c','d'],
-        answer: 'blah blah'
+        question: 'Who is Lord Commander of the Kingsguard at the beginning of Game of Thrones?',
+        choices: ['Ser Jeor Mormont','Ser Loras Tyrell','Ser Jaime Lannister','Ser Barristan Selmy'],
+        answer: 'Ser Barristan Selmy'
     },
     {
-        question: 'What is .....',
-        choices: ['a','b','c','d'],
-        answer: 'blah blah'
+        question: 'Who was the Mad King\'s firstborn son?',
+        choices: ['Viserys Targaryen','Rhaegar Targaryen','Aemon Targaryen','Aegon Targaryen'],
+        answer: 'Rhaegar Targaryen'
     },
     {
-        question: 'What is .....',
-        choices: ['a','b','c','d'],
-        answer: 'blah blah'
+        question: 'Who delivered the fatal blow to the Robb Stark?',
+        choices: ['Ramsay Bolton','Walder Frey','Alliser Thorne','Roose Bolton'],
+        answer: 'Roose Bolton'
     },
     {
-        question: 'What is .....',
-        choices: ['a','b','c','d'],
-        answer: 'blah blah'
+        question: 'Davos Seaworth grew up in the slumps of which city?',
+        choices: ['King\'s Landing','Lannisport','Gulltown','Oldtown'],
+        answer: 'King\'s Landing'
     }
 ]
 
 var clock= 5, 
-    correctAns,
-    incorAns,
-    unAns,
-    newDiv= $('<div>');
+    countDwn,
+    correctAns=0,
+    incorAns=0,
+    unAns=0,
+    pressResult,
+    pressCount=0,
+    newDiv;
 
 //Functions====================================================================
 
 //Displays the default timer count before button click 
-newDiv= newDiv.attr('class', 'hide').attr('id','countdown').text ('Time Remaining: ' + clock);
+newDiv= $('<div>').attr('class', 'hide').attr('id','countdown').text('Time Remaining: ' + clock);
 $('#timeBox').append(newDiv);
     
 //Function that hides the content until the start button is clicked 
 $('.hide').hide();
+$('.hideResults').hide();
+$('#restartButton').hide();
+$('#endMessage').hide();
 
-//When the button is clicked it will display the question and time. It will also hide and show content accordingly.  
-
+//When the button is clicked it will display the question and time. It will also hide and show content accordingly. 
 $('#startButton').on('click', function(){
     event.preventDefault();
     $('.hide').show();
     $('#startButton').hide();
     displayQues();
+    timer();
+    timerReset();
 });
 
 $(document).on('click','.options',function(){
+    pressResult= $(this).text();
+    newDiv=$('<div>');
     event.preventDefault();
+    //Conditional statements that dictate what happens depending on if the user got the question correct or not 
+    if(pressResult==quesBank[pressCount].answer){
+        newDIv= newDiv.attr('class','hide').text('You got this right!');
+        $('#message').html(newDiv);
+        correctAns++
+        $('#numCor').text(correctAns);
+        console.log(correctAns);
+        $('.hideResults').show();
+    }
+    else{
+        newDiv= newDiv.attr('class','hide').text('Sorry you got this one wrong. Focus! Here comes the next one');
+        $('#message').html(newDiv);
+        incorAns++
+        $('#numIncor').text(incorAns);
+        console.log(incorAns);
+        $('.hideResults').show();
+        
+    }
+    $('#quesChoices').data('clicked',true);
     restart();
+    pressCount++;
+
+    if(pressCount==6){
+        $('#message').hide();
+        $('#endMessage').show();
+    }
 });
+
+// variable that controls which question is displayed 
+var m= 0;
 
 //Restarts functions whenever time runs out or an answer button is clicked on
 function restart(){
-   
-    resetTimer();
+    clearButtons();
+    displayQues();
 };   
 
 // displayQues is in charge of displaying the correct questions and creating answer buttons   
 function displayQues(){
-    //need to write the function that displays new questions
-    // variable that controls which question is displayed 
-    var m= 0;
-    //variable that controls which answer choice is displayed
-    var n= 0;
-    var qBank= quesBank[m];
-    var quesVar= $('<div>').text(qBank.question);  
-    $('#quesBox').append(quesVar);
+    if(m<quesBank.length){
+        var qBank= quesBank[m];
+        //Displays the question 
+        var quesVar= $('<div>').text(qBank.question);  
+        $('#quesBox').html(quesVar);
 
-    //This loop will sort through choices, make a button, and display
-    for(x in quesBank[m].choices){
-    var newButton= $('<button>').attr('class','options').attr('id','choice ' + n).text(qBank.choices[x]);
+        //This loop will sort through choices, make a button, and display
+        for(x in quesBank[m].choices){
+        var newButton= $('<button>').attr('class','options').attr('id',qBank.choices[x]).text(qBank.choices[x]);
         $('#quesChoices').append(newButton);
-        n++;
-    };
+        };    
+
+        m++;
+    }
+    else{
+        $('#timeBox').hide();
+        $('#quesBox').hide();
+        $('#restartButton').show();
+        //Creates divs for results of the quiz ****
+    }
 };
 
-//This section of the code deals with the timing of the game 
-var countDwn= setInterval(reduceTime,1000);
+function clearButtons(){
+    $('.options').remove();
+};
+
+function timerReset(){
+    var setTimer= setInterval(timer,5000);
+    var setRestart= setInterval(restart,5000);
+    if($('#quesChoices').data('clicked')==true){
+        clearInterval(setTimer);
+        clearInterval(setRestart);
+        console.log('timerReset finally works');
+    }
+    else{
+        console.log('timer still works');
+    }
+};
 
 function timer(){
     clock= 5;
     $('#timeBox').text('Time Remaining: ' + clock);
     countDwn= setInterval(reduceTime,1000);
+    setTimeout(function(){
+        clearInterval(countDwn)}
+    ,5000);
 }; 
 
 function reduceTime(){
     clock= clock-1;
-    var newDiv= $('<div>').attr('class', 'hide').text('Time Remaining: ' + clock);
+    var newDiv= $('<div>').attr('class','hide').text('Time Remaining: ' + clock);
     $('#timeBox').html(newDiv);
 };
 
-function resetTimer(){
-    clearInterval(countDwn);
-    timer();
-};
+//========================================================================================
+//To avoid global variables, an improvement of the above would be:
 
+function setIntervalX(callback, delay, repetitions) {
+    var x = 0;
+    var intervalID = window.setInterval(function () {
 
+       callback();
+
+       if (++x === repetitions) {
+           window.clearInterval(intervalID);
+       }
+    }, delay);
+}
+//Then you can call the new setInvervalX() function as follows:
+
+// This will be repeated every for 5 times with 1 second intervals:
+setIntervalX(function () {
+    // Your logic here
+}, 1000, 5);
 
 
 
